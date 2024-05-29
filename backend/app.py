b8677bebff0be6b3ce0f617c7c959d59323e1243
Nagi-ovo <13264500190@163.com>
2024-05-29 18:43:20 +0800

    feat: reAct agent

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,86 @@
 import json
 import os
 
+from bs4 import BeautifulSoup  # noqa: F401
 from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
+from langchain_community.document_loaders import WebBaseLoader
 from litellm import completion
 
 load_dotenv()
 
-# deepseek_api_key = os.getenv('DEEPSEEK_API_KEY')
+deepseek_api_key = os.getenv('DEEPSEEK_API_KEY')
 openai_api_key = os.getenv('OPENAI_API_KEY')
 openai_base_url = os.getenv('OPENAI_API_BASE')
 
 app = Flask(__name__)
 CORS(app)
+
+IS_URL = True
+
+def deepseek_react(messages):
+    """
+    Ask DeepSeek-V2 to determine 
+    an optimal action based on the given messages.
+    
+    Parameters:
+    messages (list): A list containing dictionaries with 'role' and 'content' 
+                     representing the conversation.
+                     
+    Returns:
+    dict: A dictionary containing 'thought' (the reasoning behind the action) 
+          and 'action' (one of ["WebScrawler"]).
+    """
+
+    instruct_prompt = """
+    你是一个对网页内容总结的专家，尽可能在不减少篇幅和原有表达方式的条件下让网页内容更能够体现信息之间的关系。
+    """  # noqa: E501
+    tools = """
+    {
+        "工具": [
+            {
+                "名称": "WebCrawler",
+                "描述": "从网站中提取信息的工具",
+                "选择条件": "当用户输入是一个https:开头的网站链接而非大段文字时使用"
+            },
+            {
+                "名称": "Condenser",
+                "描述": "压缩文本",
+                "选择条件": "如果不使用WebCrawler工具才使用这个"
+            }
+        ]
+    }
+    """  # noqa: E501
+
+    react_prompt = f"""
+    你的工作是根据用户输入内容，请你经过谨慎思考选择下面的一种工具，只需回复工具名称即可(不包括引号)，提醒：大概率使用 WebCrawler:
+    {tools}
+    """  # noqa: E501
+
+    messages = [
+            {
+                "role": "user",
+                "content": f"{messages}"
+            },
+            {
+                "role": "system",
+                "content": f"{react_prompt if IS_URL else instruct_prompt}"  # noqa: E501
+            }
+        ]
+    
+    result = generate_text_completion("deepseek/deepseek-chat", messages)
+    print(result)
+    if 'error' in result:
+        return jsonify({'error': result['error']})
+
+    # Convert the content string into JSON format (assuming JSON content is returned)
+    try:
+        clean_response = result['response']
+        return clean_response
+    except Exception as e:
+        return jsonify({'error': f"Error while returning: {str(e)}"})
+
 
 def generate_text_completion(model, messages):
     """
@@ -26,13 +93,59 @@ def generate_text_completion(model, messages):
     except Exception as e:
         return {"error": f"Failed to generate response: {str(e)}"}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/api/update_graph', methods=['POST'])
-def update_graph():
-    text = request.json.get('text', '')
+def webScrawler(doc_url) -> str:
+    loader = WebBaseLoader(doc_url)
+    loader.requests_per_second = 1
+    docs = loader.aload()
+    return docs[0].page_content
+
+
+def generate_graph(text):
+    """
+    Generate a graph from the given text.
+    """
+    print("\nGenerating graph...\n")
+    one_shot_prompt = """{
+    "nodes": [
+        {
+        "data": { "id": "1", "label": "Jack" }
+        },
+        {
+        "data": { "id": "2", "label": "boy" }
+        }
+        {
+        "data": { "id": "3", "label": "horse" }
+        },
+        {
+        "data": { "id": "4", "label": "white" }
+        },
+    ],
+    "edges": [
+        {
+        "data": {
+            "source": "1",
+            "target": "2",
+            "label": "gender"
+        }
+        },
+        {
+        "data": {
+            "source": "1",
+            "target": "3",
+            "label": "is ridding"
+        }
+        },
+        {
+        "data": {
+            "source": "3",
+            "target": "4",
+            "label": "color"
+        }
+        }
+    ]
+    }
+    """
 
     messages = [
             {
@@ -47,22 +160,54 @@ def update_graph():
                   Based on the user input in various forms such as paragraph, email, text files, and more.
                   </role>
                   <task>
-                  - Your task is to create a complex knowledge graph based on the input which can excavate deep relationships.
+                  Your task is to create a complex knowledge graph based on the input which can excavate deep relationships.
                   Nodes must have a label parameter. where the label is a direct word or phrase from the input.
+                  <Example>
+                  You should learn the pattern of this example, use specific relationships instead of "is"
+                  Input: Jack is a boy who is riding a white horse.
+                  Output: {one_shot_prompt}
+                  </Example>
+                  </task>
+                  <Precautions>
                   - Edges must also have a label parameter, wher the label is a direct word or phrase from the input.
                   - Respons only with JSON in a format where we can jsonify in python and feed directly into  cy.add(data); to display a graph on the front-end.
                   Make sure the target and source of edges match an existing node.
                   - Do not include the markdown triple quotes above and below the JSON, jump straight into it with a curly bracket.
                   - Make sure that the information on the edges between nodes clearly expresses the node relationship and is not meaningless.
-                  </task>
+                  </Precautions>
                   <Your Output>
                 """  # noqa: F541, E501
             }
         ]
 
-    # result = generate_text_completion("gpt-4-0125-preview", messages)
     result = generate_text_completion("gpt-4o", messages)
-    print(result)
+    # result = generate_text_completion("deepseek/deepseek-chat", messages)
+    return result
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/update_graph', methods=['POST'])
+def update_graph():
+    global IS_URL
+    IS_URL = True
+    text = request.json.get('text', '')
+    action = deepseek_react(text) 
+    print(text, action)
+    if action == "WebCrawler":
+        print("\nUsing WebCrawler Tool......\n")
+        text = webScrawler(text)
+
+    IS_URL = False
+    if len(text) > 200:
+        print("\nCondensing......\n")
+        condensed_text = deepseek_react(text)
+    else:
+        condensed_text = text
+
+    result = generate_graph(condensed_text)
 
     if 'error' in result:
         return jsonify({'error': result['error']})
@@ -71,7 +216,7 @@ def update_graph():
     try:
         clean_response = result['response'].replace('```', '').strip()
         graph_data = json.loads(clean_response)
-        print(graph_data)
+        # print(graph_data)
         return jsonify(graph_data)
     except Exception as e:
         return jsonify({'error': f"Error parsing graph data: {str(e)}"})
